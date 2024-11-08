9cd91f9b28951c6f183d907c878ef0a0b7ecff1e
claireyyu <claireyueyuca@gmail.com>
2024-11-08 13:38:26 -0800

    Add focus functionality to name input in ProfileCard component

--- sportsphere-app/components/ProfileCard.jsx
+++ sportsphere-app/components/ProfileCard.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { COLORS, SIZE, SPACING, ROUNDED, FONTSIZE, SHADOW } from '../constants';
-import {useState} from 'react';
+import { useState, useEffect, useRef } from 'react';
   
 export default function ProfileCard({ name, email, bio }) {
   const [nameInput, setNameInput] = useState(name);
   const [emailInput, setEmailInput] = useState(email);
   const [bioInput, setBioInput] = useState(bio);
 
+  const nameInputRef = useRef(null);
+
+  useEffect(() => {
+    if (nameInputRef.current) {
+      nameInputRef.current.focus();
+    }
+  }, []);
+
   return (
     <View style={styles.cardContainer}>
       <View style={styles.profileCardRow}>
         <Text style={styles.title}>Name</Text>
-        <TextInput style={styles.infoText} value={nameInput} onChangeText={setNameInput} placeholder={name}></TextInput>
+        <TextInput ref={nameInputRef} style={styles.infoText} value={nameInput} onChangeText={setNameInput} placeholder={name} ></TextInput>
       </View>
       <View style={styles.profileCardRow}>
         <Text style={styles.title}>Email</Text>
