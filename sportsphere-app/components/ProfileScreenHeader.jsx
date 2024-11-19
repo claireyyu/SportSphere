e2df084ca3583ef2aa1ba7959cb21445dddf66ad
claireyyu <claireyueyuca@gmail.com>
2024-11-19 14:16:54 -0800

    Add location and weather functionality to ProfileScreenHeader; integrate expo-location for user location access

--- sportsphere-app/components/ProfileScreenHeader.jsx
+++ sportsphere-app/components/ProfileScreenHeader.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, Alert } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Alert, Button } from 'react-native';
 import React, { useState } from 'react';
 import DefaultScreenHeaderWrapper from './DefaultScreenHeaderWrapper';
 import { COLORS, SPACING, ROUNDED, SIZE, FONTSIZE } from '../global';
@@ -12,10 +12,49 @@ import { auth } from '../Firebase/firebaseSetup';
 import { useEffect, useContext } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { UserContext } from '../context/UserProvider';
+import * as Location from 'expo-location'
 
 export default function ProfileScreenHeader() {
   const { userProfile } = useContext(UserContext);
   const navigation = useNavigation();
+  const [response, requestPermission] = Location.useForegroundPermissions();
+  const [location, setLocation] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  async function verifyPermission() {
+    try {
+      if (!response.granted) {
+        const granted = await requestPermission();
+        return granted.granted;
+      }
+      return true;
+    } catch {
+      console.log("Permission denied");
+      return false;
+    }
+  }
+
+  async function locateUserHandler() {
+    try {
+      const hadPermission = await verifyPermission();
+      if (!hadPermission) {
+        Alert.alert("You need to give location permissions to use this feature.");
+        return;
+      }
+      const result = await Location.getCurrentPositionAsync();
+      setLocation({ latitude: result.coords.latitude, longitude: result.coords.longitude });
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    if (location) {
+      console.log("location: ", location);
+    }
+  }, [location]);
+
   
   async function signOutUser() {
     try {
@@ -48,6 +87,23 @@ export default function ProfileScreenHeader() {
     navigation.navigate("Reminder");
   }
 
+  async function weatherHandler() {
+    try {
+      await locateUserHandler();
+      if (location) {
+        const { latitude: lat, longitude: lon } = location;
+        const part = "minutely,hourly,daily";
+        const APIkey = "6ed1072d831e6cda74d4789206f6975b";
+        const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${APIkey}`);
+        const data = await response.json();
+        console.log("weather!!!!!: ", data);
+        // setWeather({temp: data., description: data.current.weather[0].main});
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <SafeAreaView style={styles.view}>
       <View style={styles.topBtnContainer}>
@@ -74,7 +130,15 @@ export default function ProfileScreenHeader() {
           source={{ uri: "https://avatar.iran.liara.run/public/girl" }}
         />
         <View style={styles.profileInfo}>
+          <View style={styles.firstLineContainer}>
           <Text style={styles.username}>{userProfile?.username || 'User Name'}</Text>
+            <PressableButton
+              componentStyle={styles.weather}
+              pressedFunction={weatherHandler}
+            >
+              <Text style={styles.weatherText}>{weather || "Weather"}</Text>
+            </PressableButton>
+          </View>
           <Text style={styles.email}>{userProfile?.email || 'User Email'}</Text>
           <Text style={styles.bio}>{userProfile?.bio || 'User Bio'}</Text>
           {/* <Text style={styles.bio}>{userProfile?.userDocId || 'User userDocId'}</Text> */}
@@ -125,6 +189,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginHorizontal: SPACING.default,
   },
+  firstLineContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
   profileInfo: {
     justifyContent: 'center',
     marginLeft: SPACING.default,
@@ -133,6 +202,16 @@ const styles = StyleSheet.create({
     color: COLORS.background,
     fontSize: FONTSIZE.h1,
     fontWeight: 'bold',
+  },
+  weather: {
+    backgroundColor: COLORS.edit,
+    borderRadius: ROUNDED.small,
+    padding: SPACING.xsmall,
+    marginLeft: SPACING.default,
+  },
+  weatherText: {
+    color: COLORS.background,
+    fontSize: FONTSIZE.tiny,
   },
   email: {
     color: COLORS.defaultBackground,
