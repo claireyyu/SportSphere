4ad9785c5aed2c9682713ca92fe2dbf484d755dd
claireyyu <claireyueyuca@gmail.com>
2024-11-28 15:18:34 -0800

    Refactor JoinedActivitiesScreen; enhance layout with navigation button and title, and integrate ProfileActivityCardList component

--- sportsphere-app/screens/JoinedActivitiesScreen.jsx
+++ sportsphere-app/screens/JoinedActivitiesScreen.jsx
@@ -1,13 +1,44 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { COLORS } from '../global'
+import ChatCard from '../components/ChatCard'
+import { COLORS, SPACING, FONTSIZE, SIZE } from '../global'
+import ProfileActivityCardList from '../components/ProfileActivityCardList'
+import PressableButton from '../components/PressableButton'
+import { Ionicons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 
 export default function JoinedActivitiesScreen() {
+  const navigation = useNavigation();
+  
   return (
-    <View style={{flex: 1, backgroundColor: COLORS.themeLight, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color: COLORS.theme}}>JoinedActivitiesScreen</Text>
+    <View style={styles.container}>
+      <PressableButton
+        pressedFunction={() => navigation.goBack()}
+        componentStyle={{marginTop: SPACING.xxl}}
+      >
+        <Ionicons name="chevron-back-sharp" size={SIZE.pressableIcon} color={COLORS.theme} />
+      </PressableButton>
+      <View>
+        <Text style={styles.title}>Joined Activities</Text>
+      </View>
+      <View style={{marginTop: SPACING.l}}>
+        <ProfileActivityCardList />
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // marginHorizontal: SPACING.medium,
+    backgroundColor: COLORS.themeLight,
+    padding: SPACING.l,
+  },
+
+  title: {
+    fontFamily: 'Montserrat_700Bold',
+    fontSize: FONTSIZE.h0,
+    marginTop: SPACING.xxl,
+  }
+})
